cf431c3a81faf2b52ba7cf1f596d2be47b36924c
朱季葳 <wewe5215@zhujiweideMacBook-Pro.local>
2023-11-16 00:40:27 +0800

    fix: remove SIMDE_NATURAL_VECTOR_SIZE >= 128

--- simde/arm/neon/addw_high.h
+++ simde/arm/neon/addw_high.h
@@ -49,7 +49,7 @@ simde_vaddw_high_s8(simde_int16x8_t a, simde_int8x16_t b) {
     simde_int16x8_private a_ = simde_int16x8_to_private(a);
     simde_int8x16_private b_ = simde_int8x16_to_private(b);
 
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vint8mf2_t b_high = __riscv_vlmul_trunc_v_i8m1_i8mf2(__riscv_vslidedown_vx_i8m1(b_.sv128 , 8 , 16));
       r_.sv128 =  __riscv_vwadd_wv_i16m1(a_.sv128, b_high, 8);
     #else
@@ -79,7 +79,7 @@ simde_vaddw_high_s16(simde_int32x4_t a, simde_int16x8_t b) {
     simde_int32x4_private a_ = simde_int32x4_to_private(a);
     simde_int16x8_private b_ = simde_int16x8_to_private(b);
 
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vint16mf2_t b_high = __riscv_vlmul_trunc_v_i16m1_i16mf2(__riscv_vslidedown_vx_i16m1(b_.sv128 , 4 , 8));
       r_.sv128 =  __riscv_vwadd_wv_i32m1(a_.sv128, b_high, 4);
     #else
@@ -108,7 +108,7 @@ simde_vaddw_high_s32(simde_int64x2_t a, simde_int32x4_t b) {
     simde_int64x2_private r_;
     simde_int64x2_private a_ = simde_int64x2_to_private(a);
     simde_int32x4_private b_ = simde_int32x4_to_private(b);
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vint32mf2_t b_high = __riscv_vlmul_trunc_v_i32m1_i32mf2(__riscv_vslidedown_vx_i32m1(b_.sv128 , 2 , 4));
       r_.sv128 =  __riscv_vwadd_wv_i64m1(a_.sv128, b_high, 2);
     #else
@@ -136,7 +136,7 @@ simde_vaddw_high_u8(simde_uint16x8_t a, simde_uint8x16_t b) {
     simde_uint16x8_private r_;
     simde_uint16x8_private a_ = simde_uint16x8_to_private(a);
     simde_uint8x16_private b_ = simde_uint8x16_to_private(b);
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vuint8mf2_t b_high = __riscv_vlmul_trunc_v_u8m1_u8mf2(__riscv_vslidedown_vx_u8m1(b_.sv128 , 8 , 16));
       r_.sv128 =  __riscv_vwaddu_wv_u16m1(a_.sv128, b_high, 8);
     #else
@@ -164,7 +164,7 @@ simde_vaddw_high_u16(simde_uint32x4_t a, simde_uint16x8_t b) {
     simde_uint32x4_private r_;
     simde_uint32x4_private a_ = simde_uint32x4_to_private(a);
     simde_uint16x8_private b_ = simde_uint16x8_to_private(b);
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vuint16mf2_t b_high = __riscv_vlmul_trunc_v_u16m1_u16mf2(__riscv_vslidedown_vx_u16m1(b_.sv128 , 4 , 8));
       r_.sv128 =  __riscv_vwaddu_wv_u32m1(a_.sv128, b_high, 4);
     #else
@@ -192,7 +192,7 @@ simde_vaddw_high_u32(simde_uint64x2_t a, simde_uint32x4_t b) {
     simde_uint64x2_private r_;
     simde_uint64x2_private a_ = simde_uint64x2_to_private(a);
     simde_uint32x4_private b_ = simde_uint32x4_to_private(b);
-    #if defined(SIMDE_RISCV_V_NATIVE) && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
+    #if defined(SIMDE_RISCV_V_NATIVE)
       vuint32mf2_t b_high = __riscv_vlmul_trunc_v_u32m1_u32mf2(__riscv_vslidedown_vx_u32m1(b_.sv128 , 2 , 4));
       r_.sv128 =  __riscv_vwaddu_wv_u64m1(a_.sv128, b_high, 2);
     #else

aaa910eab969d40e74214205753d975fd95a49fb
Ruhung <143302514+Ruhung@users.noreply.github.com>
2023-11-20 14:29:48 +0800

    Revert "fix:ld4, mf2 -> m1"
    This reverts commit 6ab9846733b7d57774d79838416d580f02b60701.

--- simde/arm/neon/ld4.h
+++ simde/arm/neon/ld4.h
@@ -50,10 +50,10 @@ simde_vld4_f16(simde_float16_t const ptr[HEDLEY_ARRAY_PARAM(16)]) {
     simde_float16x4_private a_[4];
     #if defined(SIMDE_RISCV_V_NATIVE) && SIMDE_ARCH_RISCV_ZVFH && (SIMDE_NATURAL_VECTOR_SIZE >= 128)
       vfloat16m1x4_t dest = __riscv_vlseg4e16_v_f16m1x4((_Float16 *)&ptr[0], 4);
-      a_[0].sv64 = __riscv_vget_v_f16m1x4_f16mf1(dest, 0);
-      a_[1].sv64 = __riscv_vget_v_f16m1x4_f16mf1(dest, 1);
-      a_[2].sv64 = __riscv_vget_v_f16m1x4_f16mf1(dest, 2);
-      a_[3].sv64 = __riscv_vget_v_f16m1x4_f16mf1(dest, 3);
+      a_[0].sv64 = __riscv_vget_v_f16m1x4_f16mf2(dest, 0);
+      a_[1].sv64 = __riscv_vget_v_f16m1x4_f16mf2(dest, 1);
+      a_[2].sv64 = __riscv_vget_v_f16m1x4_f16mf2(dest, 2);
+      a_[3].sv64 = __riscv_vget_v_f16m1x4_f16mf2(dest, 3);
     #else
       for (size_t i = 0; i < (sizeof(simde_float16x4_t) / sizeof(*ptr)) * 4 ; i++) {
         a_[i % 4].values[i / 4] = ptr[i];
